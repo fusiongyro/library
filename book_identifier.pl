d2574524bd71f302cc65e2f87caab0365ab6211a
Daniel Lyons <fusion@storytotell.org>
2014-04-22 11:02:43 -0600

    various means of squishing a book identifier out of a title

--- book_identifier.pl
+++ book_identifier.pl
@@ -1,27 +1,100 @@
 :- module(book_identifier, [suggest_identifier/2]).
 
+:- use_module(library(dcg/basics)).
+
 % remove_whitespace(+Title, ?CompressedTitle) is semidet.
 %
 %   True if Title contains whitespace and CompressedTitle is Title
 %   without the whitespace.
-remove_whitespace(Title, CompressedTitle) :- fail.
+remove_whitespace(Title, CompressedTitle) :-
+    atom(Title), atom_codes(Title, TitleChars),
+    remove_whitespace(TitleChars, CompressedTitleChars),
+    atom_codes(CompressedTitle, CompressedTitleChars).
+remove_whitespace([], []).
+remove_whitespace([T|Ts], Rest) :-
+    (char_type(T, graph)
+       -> (remove_whitespace(Ts, Remaining),
+           Rest = [T|Remaining])
+       ;  (remove_whitespace(Ts, Rest))).
+
+
+% word_tokens(+Title, ?WordTokens) is det.
+%
+%   Unifies WordTokens with a list of words from Title.
+word_tokens(Word, Tokens) :-
+    atom(Word),
+    atom_codes(Word, WordCodes),
+    word_tokens(WordCodes, Tokens).
+word_tokens(WordCodes, Tokens) :-
+    is_list(WordCodes),
+    once(phrase(word_tokens(Tokens), WordCodes)).
+
+word_tokens([Word]) -->
+    nonblanks(WordCodes), 
+    { atom_codes(Word, WordCodes) }.
+word_tokens([Word|Words]) -->
+    nonblanks(WordCodes),
+    whites,
+    word_tokens(Words), 
+    { atom_codes(Word, WordCodes) }.
+
+
+% remove_matching(:Goal, +Title, ?CompressedTitle) is det.
+%
+%   True if CompressedTitle is a title with all words matching Goal
+%   removed.
+remove_matching(Goal, Title, CompressedTitle) :-
+    atom(Title),
+    word_tokens(Title, TitleWords),
+    remove_matching(Goal, TitleWords, CompressedTitleWords),
+    atomic_list_concat(CompressedTitleWords, ' ', CompressedTitle).
+remove_matching(_, [], []).
+remove_matching(Goal, [Word|TitleRest], [Word|Remaining]) :-
+    \+ (call(Goal, Article), string_lower(Word, Article)),
+    remove_matching(Goal, TitleRest, Remaining).
+remove_matching(Goal, [ArticleT|TitleRest], Remaining) :-
+    string_lower(ArticleT, Article),
+    call(Goal, Article),
+    remove_matching(Goal, TitleRest, Remaining).
+
+
+% support for remove_articles
+article(a). article(an).
+article(the).
 
 % remove_articles(+Title, ?CompressedTitle) is semidet.
 %
 %   True if Title contains articles (a, an, the) and CompressedTitle
 %   is Title without the articles.
-remove_articles(Title, CompressedTitle) :- fail.
+remove_articles(Title, CompressedTitle) :-
+    remove_matching(article, Title, CompressedTitle).
+
+% support for remove_grammatical_words
+grammar(and).	grammar(from).	grammar(onto).
+grammar(but).	grammar(in).	grammar(of).
+grammar(by).	grammar(over).	grammar(to).
+grammar(for).	grammar(on).	grammar(unto).
 
 % remove_grammatical_words(+Title, ?CompressedTitle) is semidet.
 %
 %   True if Title contains grammatical words (the, of, to, etc.) and
 %   CompressedTitle is Title without the grammatical words.
-remove_grammatical_words(Title, CompressedTitle) :- fail.
+remove_grammatical_words(Title, CompressedTitle) :-
+    remove_matching(article, Title, ArticlelessTitle),
+    remove_matching(grammar, ArticlelessTitle, CompressedTitle).
 
 % take_initials(+Title, ?Initials) is det.
 %
 %   Unifies Initials with the initial letter of each word of Title.
-take_initials(Title, Initials) :- fail.
+take_initials(Title, Initials) :-
+    atom(Title),
+    word_tokens(Title, TitleWords),
+    take_initials(TitleWords, InitialChars),
+    atomic_list_concat(InitialChars, Initials).
+take_initials([], []).
+take_initials([Word|Words], [Initial|Initials]) :-
+    atom_chars(Word, [Initial|_]),
+    take_initials(Words, Initials).
 
 % take_prefixes(+Title, +TargetLength, ?Prefixes) is det.
 %
@@ -29,7 +102,22 @@ take_initials(Title, Initials) :- fail.
 %   Title to produce a single prefix list.
 %
 %   Example: "Programming Languages", 6 -> "ProLan".
-take_prefixes(Title, TargetLength, Prefixes) :- fail.
+take_prefixes(Title, TargetLength, Prefixes) :-
+    atom(Title),
+    word_tokens(Title, TitleWords),
+    take_prefixes(TitleWords, TargetLength, PrefixWords),
+    atomic_list_concat(PrefixWords, Prefixes).
+take_prefixes(TitleList, TargetLength, Prefixes) :-
+    length(TitleList, N),
+    PerWord is ceil(TargetLength / N),
+    take_prefixes(TitleList, TargetLength, PerWord, Prefixes).
+take_prefixes([], _, _, []).
+take_prefixes(_, Target, _, []) :- Target =< 0.
+take_prefixes([Word|Title], Target, PerWord, [Next|Prefixes]) :-
+    Len is min(Target, PerWord), Len > 0,
+    sub_atom(Word, 0, Len, _, Next),
+    NewTarget is Target - Len,
+    take_prefixes(Title, NewTarget, PerWord, Prefixes).
 
 % suggest_identifier(+Title, -SuggestedIdentifier) is multi.
 %
@@ -40,9 +128,17 @@ suggest_identifier(Title, SuggestedIdentifier) :- fail.
 % Test cases
 :- use_module(library(plunit)).
 
+:- begin_tests(word_tokens).
+
+test(wt_theend) :- word_tokens('This is the End', [this,is,the,end]).
+test(wt_hhiker) :- word_tokens('The Hitchhiker''s Guide to the Galaxy', [the,'hitchhiker''s',guide,to,the,galaxy]).
+
+:- end_tests(word_tokens).
+
 :- begin_tests(remove_whitespace).
 
-test(whitespace) :- remove_whitespace('This is the End', 'ThisistheEnd').
+test(whitespace1) :- remove_whitespace('This is the End', 'ThisistheEnd').
+test(whitespace2) :- remove_whitespace('This    is\t the End\n\n', 'ThisistheEnd').
 
 :- end_tests(remove_whitespace).
 
@@ -57,6 +153,7 @@ test(a)   :- remove_articles('A Christmas Story', 'Christmas Story').
 :- begin_tests(remove_grammatical_words).
 
 test(to) :- remove_grammatical_words('The Hitchhiker''s Guide to the Galaxy', 'Hitchhiker''s Guide Galaxy').
+test(startwar) :- remove_grammatical_words('The start of a war', 'start war').
 
 :- end_tests(remove_grammatical_words).
 
